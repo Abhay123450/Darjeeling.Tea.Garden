84fb40ad6542c083a7e40e0430c351c574b04260
abhay <abhay123450@gmail.com>
2026-01-12 21:03:02 +0530

    update: dependencies

--- app/src/main/java/com/darjeelingteagarden/database/CartRepository.kt
+++ app/src/main/java/com/darjeelingteagarden/database/CartRepository.kt
@@ -1,16 +1,16 @@
-package com.darjeelingteagarden.database
-
-import androidx.lifecycle.LiveData
-
-class CartRepository(private val cartDao: CartDao) {
-
-    val readCartData: LiveData<List<CartEntity>> = cartDao.getCartItems()
-
-    suspend fun addItem(cartEntity: CartEntity){
-        cartDao.addToCart(cartEntity)
-    }
-
-    fun itemExistsInCart(product_id: String){
-        cartDao.getItemById(product_id)
-    }
-}
+//package com.darjeelingteagarden.database
+//
+//import androidx.lifecycle.LiveData
+//
+//class CartRepository(private val cartDao: CartDao) {
+//
+//    val readCartData: LiveData<List<CartEntity>> = cartDao.getCartItems()
+//
+//    suspend fun addItem(cartEntity: CartEntity){
+//        cartDao.addToCart(cartEntity)
+//    }
+//
+//    fun itemExistsInCart(product_id: String){
+//        cartDao.getItemById(product_id)
+//    }
+//}
